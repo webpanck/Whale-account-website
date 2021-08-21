9506631ad44e1c41857fd7d8ee694a325c65aa0b
潘诚宽 <chengkuan0327@163.com>
2021-08-21 21:38:05 +0800

    deploy

---
+++ precache-manifest.66ac6f10b8dd9b0c37070757e3c59504.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d195b70a4701ce077122",
+    "url": "/Whale-account-website/css/app.6df823a2.css"
+  },
+  {
+    "revision": "da60f3c0737a4fcd725a09a8de53161a",
+    "url": "/Whale-account-website/index.html"
+  },
+  {
+    "revision": "d195b70a4701ce077122",
+    "url": "/Whale-account-website/js/app.1bc17d96.js"
+  },
+  {
+    "revision": "2a841d3865aef3460381",
+    "url": "/Whale-account-website/js/chunk-vendors.c2656864.js"
+  },
+  {
+    "revision": "b6d8282f1cd23cfad18b6454e5b25c1d",
+    "url": "/Whale-account-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Whale-account-website/robots.txt"
+  }
+]);
